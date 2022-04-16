4d5cdb09d879125f843533f298fbbe9653c1aaf2
Margarita Delikatnaya <rita.delikatnaya@mail.ru>
2022-04-16 16:14:29 +0300

    fix server js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
-
+const express = require("express");
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.png"));
-
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/ping", function (req, res) {
-  return res.send("pong");
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.listen(port, () => {
+  console.log("Server is up!");
 });
-app.listen(port);
